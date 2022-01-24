bd386391cb9164fd6caf5de13e3e1bd2df063fe3
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2022-01-24 09:52:18 -0300

    Login form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,33 @@
 import type { NextPage } from 'next'
 
+import { Button, Flex, Stack } from '@chakra-ui/react'
+
+import { Input } from '../components/Form/Input'
+
 const Home: NextPage = () => {
-  return <h1>Dashgo</h1>
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" label="E-mail" type="email" />
+
+          <Input name="password" label="Senha" type="password" />
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  )
 }
 
 export default Home
