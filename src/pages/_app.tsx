19303a1c844633c3d292ca115fede80b2efc5b08
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2022-01-24 21:16:53 -0300

    Configure react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 import { ChakraProvider } from '@chakra-ui/react'
 
@@ -10,13 +11,17 @@ if (process.env.NODE_ENV === 'development') {
   makeServer()
 }
 
+const queryClient = new QueryClient()
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   )
 }
 
